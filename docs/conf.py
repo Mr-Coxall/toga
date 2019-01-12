e0ce541617018b8ae64f12ea70b5942fd64a297c
Mr Coxall <mr.coxall@mths.ca>
2019-01-11 22:22:25 -0500

    removed another old chapter

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Computer Based Problem Solving'
 copyright = u'2020, Patrick Coxall'
+author = u'Patrick Coxall' 
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
