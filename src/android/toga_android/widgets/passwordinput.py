be6b3d1e953dd6ecb4df4a53d61953d3861d5b22
Mr Coxall <mr.coxall@mths.ca>
2019-01-11 21:33:54 -0500

    removed the src directory

--- src/android/toga_android/widgets/passwordinput.py
+++
@@ -1,8 +0,0 @@
-from .textinput import TextInput, TogaTextInput
-
-
-class PasswordInput(TextInput):
-    def create(self):
-        super().create()
-        self.native.inputType = 'textPassword'
-
--- src/dummy/toga_dummy/command.py
+++
@@ -1,7 +0,0 @@
-from toga_dummy.utils import LoggedObject
-
-
-class Command(LoggedObject):
-    def __init__(self, interface):
-        super().__init__()
-        self.interface = interface        
